257fa0f183858a9f7b5d757564a64a9681aac245
PropeReferio <lemuel.b.stevens@gmail.com>
2020-11-16 23:07:16 -0600

    environment vars working

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import requests
 from flask import Flask, render_template, request
 from calculate import kelvin_to_fahr
-from dotenv import load_dotenv
-load_dotenv()
 import os
-# How we call to APIs and other URLs
 
 app = Flask(__name__)
-os.get_env("api_key")
+api_key = os.environ.get('OPENWEATHER_API_KEY')
+print('api_key:', api_key)
 
 @app.route('/temperature', methods=['POST'])
 def temperature():
 	if 'zip' in request.form:
 		zipcode = request.form['zip']
-		r = requests.get(f'http://api.openweathermap.org/data/2.5/weather?zip={zipcode},us&appid={api_key}').json()
+		r = requests.get(f'http://api.openweathermap.org/data/2.5/weather?zip={zipcode},us&appid={api_key}')
+		return r.text
 		return render_template('temperature.html', f"{kelvin_to_fahr(r):.1f}")
 	
 	elif 'city' in request.form and 'state' in request.form:
